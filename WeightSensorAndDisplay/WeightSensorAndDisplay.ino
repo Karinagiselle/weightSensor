6431d4dc6ee4a576de4a884ab70ebdf1cb854b3a
Karina Blanco <karinagiselle.b@gmail.com>
2020-10-18 17:48:10 +0100

    Add working weight sensor script

---
+++ WeightSensorAndDisplay/WeightSensorAndDisplay.ino
@@ -0,0 +1,76 @@
+#include "HX711.h"
+#include <ESP8266WiFi.h>;
+//#include <WiFiClient.h>;
+#include <ThingSpeak.h>;
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+HX711 scale;
+
+float calibration_factor = 2300; // this calibration factor is adjusted according to my load cell
+int units;
+float ounces;
+
+void setup() 
+{
+  Serial.begin(115200);
+  Wire.begin(D2, D1);
+    
+  lcd.init();
+  lcd.backlight();
+  lcd.print("NodeMCU Wg Scale");
+  
+  scale.begin(D5, D6);
+  scale.set_scale();
+  scale.tare();  //Reset the scale to 0
+
+  long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
+
+}
+
+void loop() 
+
+{
+
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+
+  Serial.print("Reading: ");
+  lcd.clear();
+    
+  units = scale.get_units(), 10;
+
+  if (units < 0)
+  {
+    units = 0;
+  }
+ 
+  if (units == 0)
+  {
+       lcd.print("Weight :");
+       lcd.print(units);
+       lcd.print("  g");
+       Serial.print(units);
+       Serial.print(" grams"); 
+       Serial.print(" calibration_factor: ");
+       Serial.print(calibration_factor);
+       Serial.println();
+       delay(1000);
+   } 
+  
+  if (units > 0)
+  {
+       lcd.print("Weight :");
+       lcd.print(units);
+       lcd.print("  g");
+       Serial.print(units);
+       Serial.print(" grams"); 
+       Serial.print(" calibration_factor: ");
+       Serial.print(calibration_factor);
+       Serial.println();
+       delay(1000);
+  } 
+}
