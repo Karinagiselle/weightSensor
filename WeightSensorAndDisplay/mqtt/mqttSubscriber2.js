627d6b6135b2069dc30dc9a0c9d09edd18dea73c
Karina Blanco <karinagiselle.b@gmail.com>
2020-12-01 16:01:13 +0000

    Add subscriber for my local MQTT

---
+++ WeightSensorAndDisplay/mqtt/mqttSubscriber2.js
@@ -0,0 +1,23 @@
+var mqtt = require('mqtt'); // https://github.com/mqttjs/MQTT.js
+
+var topic = "esp/welcome";
+
+// default message
+var message   = '0';
+
+if (process.argv.length > 2) {
+  // use command line argument as message
+  message = process.argv[2]
+}
+var client  = mqtt.connect('mqtt://127.0.0.1')
+
+client.on('connect', function () {
+  client.subscribe(topic)
+  // client.publish('presence', 'Hello mqtt')
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  client.end()
+})
